c23c52af3da798157bd3c9d4be14f0c1f5f05b6f
sumit-singh-dhalla <cse2011.sumit@gmail.com>
2023-07-07 16:10:49 +0530

    updted exercise1

--- exercise1-line-chart.py
+++ exercise1-line-chart.py
@@ -1,44 +1,18 @@
 from matplotlib import pyplot as plt
 
+plt.style.use("fivethirtyeight")
+# plt.xkcd()
 
 dev_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 dev_y = [35000, 37456, 36785, 37890, 40455, 40238, 41675, 42545, 42986, 43958, 43987]
 py_dev_y = [45000, 47456, 47785, 48890, 49455, 50238, 51675, 53545, 53986, 53958, 54987]
 
+plt.plot(dev_x, dev_y, linewidth=5, label="all dev")
+plt.plot(dev_x, py_dev_y, linewidth=5, label="python dev")
 
-plt.plot(dev_x, dev_y, label="All Dev", c="r")
-plt.plot(dev_x, py_dev_y, label="Python", c="g")
 plt.xlabel("Ages")
-plt.ylabel("salaries(INR)")
-plt.title("Dev salaries as per their ages")
+plt.ylabel("Salaries(INR)")
+plt.title("Dev Salaries as per ages")
 plt.legend()
-plt.grid()
+# plt.grid(True)
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-plt.style.use("fivethirtyeight")
-plt.xkcd()
-py_dev_y = [45000, 47456, 47785, 48890, 49455, 50238, 51675, 53545, 53986, 53958, 54987]
-plt.plot(dev_x, py_dev_y, label="Python dev")
-plt.xlabel("ages")
-plt.ylabel("Salary (INR)")
-plt.title("Developer Salary (INR) as per ages")
-
-plt.legend()
-plt.grid(True)
-"""
